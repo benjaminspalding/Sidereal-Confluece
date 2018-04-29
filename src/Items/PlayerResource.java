d9a98b3ea662c03b5968389cc72448dcfca9ad37
benjaminspalding <benjamin.spaling02@gmail.com>
2018-04-29 14:45:22 -0400

    added additional constructor to PlayerResource class

--- src/Items/PlayerResource.java
+++ src/Items/PlayerResource.java
@@ -1,15 +1,16 @@
 package Items;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
+
 
 public class PlayerResource {
 
-    private Map<Resource, Integer> resources = new HashMap<>();
+    private LinkedHashMap<Resource, Integer> resources = new LinkedHashMap<>();
+
 
     PlayerResource(){
-        resources.put(new SmallCube(SmallCubeColor.BROWN), 0); // <- do this for all cubes
-        resources.put((new SmallCube(SmallCubeColor.GREEN), 0);
+        resources.put(new SmallCube(SmallCubeColor.BROWN), 0);
+        resources.put(new SmallCube(SmallCubeColor.GREEN), 0);
         resources.put(new SmallCube(SmallCubeColor.WHITE), 0);
         resources.put(new SmallCube(SmallCubeColor.GREY), 0);
         resources.put(new BigCube(BigCubeColor.BLACK), 0);
@@ -19,16 +20,45 @@ public class PlayerResource {
         resources.put(new Ultratech(), 0);
         resources.put(new Ship(), 0);
         resources.put(new VictoryPoint(), 0);
-
     }
 
-    public PlayerResource add(){
+
+    PlayerResource(int smlBrownCube, int smlGreenCube, int smlWhiteCube, int smlGreyCube,
+                   int bigBlackCube, int bigBlueCube, int bigYellowCube, int bigGreyCube,
+                   int ultraTech, int ships, int vp){
+        resources.put(new SmallCube(SmallCubeColor.BROWN), smlBrownCube);
+        resources.put(new SmallCube(SmallCubeColor.GREEN), smlGreenCube);
+        resources.put(new SmallCube(SmallCubeColor.WHITE), smlWhiteCube);
+        resources.put(new SmallCube(SmallCubeColor.GREY), smlGreyCube);
+        resources.put(new BigCube(BigCubeColor.BLACK), bigBlackCube);
+        resources.put(new BigCube(BigCubeColor.BLUE), bigBlueCube);
+        resources.put(new BigCube(BigCubeColor.YELLOY), bigYellowCube);
+        resources.put(new BigCube(BigCubeColor.GREY), bigGreyCube);
+        resources.put(new Ultratech(), ultraTech);
+        resources.put(new Ship(), ships);
+        resources.put(new VictoryPoint(), vp);
+    }
+
+    // This is private so that we don't expose the map backing field to the world
+    private LinkedHashMap<Resource, Integer> getPlayerResourceMap(){
+        return resources;
+    }
+
+    public PlayerResource add(PlayerResource reqRes){
         //TODO: implement adding, the method header and definition are not complete.
         //  This might be a little bit of work.
 
-        // putall method. Figure out houw to put hashmaps as arguments
+        //declaring variables for iterating over the loop
+        Set<Resource> resourceKeys = resources.keySet();
+        Collection<Integer> resourceVals = resources.values();
 
-        return null;
+        //use a real for loop pussy
+        for(Resource keys:resourceKeys){
+
+        }
+
+            return null;
+
     }
 
     public PlayerResource subtract(){
---
+++ src/Program.java
@@ -0,0 +1,6 @@
+public class Program {
+    public static void main(String[] args){
+        // do the program here
+        System.out.println("Hello from main");
+    }
+}
